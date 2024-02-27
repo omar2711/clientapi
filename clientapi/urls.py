d433002d742b165658bd78bd65bef527ffa623f7
omar2711 <omar11santiago@hotmail.com>
2024-02-27 18:08:02 -0400

    commit

--- clientapi/urls.py
+++ clientapi/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path, include
 from rest_framework import routers
 from clientapi import views
+from .views import serverView
 
 router = routers.DefaultRouter()
 router.register(r'server', views.serverView)
 
 urlpatterns = [
-    path('getinfo/', views.serverView.obtener_informacion_disco_y_pid, name='obtener_informacion_disco_y_pid'),
-
+    path('', include(router.urls)),
+    path('getinfo/', serverView.as_view({'get': 'obtener_informacion_disco_y_pid'}), name='obtener_informacion_disco_y_pid'),
 ]
+
 
